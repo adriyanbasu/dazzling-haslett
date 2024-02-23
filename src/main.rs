2405ec950641d515cbbca88bde69935a11e7c1b8
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-23 00:34:57 +0000

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::vec;
+
+enum Errors {}
+
+enum TokenType {}
+
+struct Token {
+    pub ty: TokenType,
+    pub value: String,
+}
+
+fn tokenize(code: &str) -> Result<Vec<Token>, Errors> {
+    let tokens = vec::new();
+    let curent_token: Option<Token> = None;
+    for c in code.chars() {
+        if c == ';' {}
+    }
+    Ok(tokens)
+}
+
 fn main() {
-    println!("Hello CodeSandbox!");
+    let tokens = tokenize("x = 2;").unwrap();
+
+    println!("{:?}", tokens);
 }
