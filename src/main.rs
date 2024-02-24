55621729d2a1c16ebedc1804300e88ec03cfeb13
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-02-24 21:50:48 +0000

    a commit

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::vec;
+use std::{string, vec};
 
+#[derive(Debug)]
 enum Errors {}
 
-enum TokenType {}
+enum TokenType {
+    SemiColon,
+}
 
 struct Token {
     pub ty: TokenType,
     pub value: String,
+    pub errors: Errors,
 }
 
 fn tokenize(code: &str) -> Result<Vec<Token>, Errors> {
     let tokens = vec::new();
     let curent_token: Option<Token> = None;
     for c in code.chars() {
-        if c == ';' {}
+        if c == ';' {
+            if let Some(tok) = curent_token {
+                tokens.push(tok);
+            }
+            tokens.push(Token {
+                ty: TokenType::SemiColon,
+                value: string::from(';'),
+                errors: Errors {},
+            });
+            curent_token = None;
+        }
     }
     Ok(tokens)
 }
